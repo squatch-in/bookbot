1b26f9d30b4064675fef2806b968a1c642e9071a
squatch-in <jvwyatt&@gmail.com>
2024-12-08 20:15:42 -0600

    added a function that opens frankenstein file and counts the words in file

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+#This function opens a file and returns and prints the contents of the file.
+def main():
+    with open ("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        return file_contents  
+book = main()
+print(book)
+
+#This counts the number of words from the book function.
+number_of_words = len(book.split())
+print(number_of_words)
+
