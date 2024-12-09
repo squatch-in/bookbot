19a9ae3a2f75340d5bdf4e7f120049eed18aebe8
squatch-in <jvwyatt&@gmail.com>
2024-12-08 22:38:10 -0600

    working on character count

--- main.py
+++ main.py
@@ -4,9 +4,26 @@ def main():
         file_contents = f.read()
         return file_contents  
 book = main()
-print(book)
+# print(book)
 
 #This counts the number of words from the book function.
 number_of_words = len(book.split())
 print(number_of_words)
 
+# The book in all lowercase
+lower_case_book = book.lower()
+# print(lower_case_book)
+
+def character_count():
+    all_characters = {}
+    unique_set = set()
+    for character in lower_case_book:
+        if character not in unique_set:
+            unique_set.add(character)
+            
+    
+    
+
+    
+
+character_count()
